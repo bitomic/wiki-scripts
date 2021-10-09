bd7b55bf106325ec10370d14cc8175a8735cc147
bitomic <35199700+bitomic@users.noreply.github.com>
2021-10-09 16:04:57 -0600

    feat: get yugioh cards information and dump it in a json file

--- packages/es.yugiohdecks/src/Cards.ts
+++ packages/es.yugiohdecks/src/Cards.ts
@@ -1,6 +1,9 @@
 import { Fandom } from 'mw.js'
+import fs from 'fs-extra'
 import { parse } from 'mwparser'
-import util from 'util'
+import path from 'path'
+import type { Template } from 'mwparser'
+//import util from 'util'
 
 /*
 ( async () => {
@@ -15,33 +18,113 @@ import util from 'util'
 } )()
 */
 
+const getIdentifier = ( name: string ): string => name.toUpperCase()
+	.normalize( 'NFD' )
+	.replace( /[\u0300-\u036f]/g, '' )
+	.replace( /\((legal|carta|card)\)/i, '' )
+	.replace( /[^A-ZÑ0-9]/g, '' )
+
+const parseCard = ( infobox: Template ): string[] | null => {
+	const message = infobox.getParameter( 'mensaje' )?.value
+	if ( message?.toLowerCase().includes( 'anime' ) ) return null
+	const cardType = infobox.getParameter( 'carta' )?.value.toLowerCase()
+	if ( !cardType ) return null
+	if ( [
+		'monstruo ra', 'monstruo slifer', 'monstruo obelisco', 'monstruo de sincronía oscura', 'ficha', 'carta de virus'
+	].includes( cardType ) ) return null
+
+	if ( cardType === 'carta mágica' ) {
+		const attributes = [
+			'inglés', 'icono', 'código'
+		]
+		const [
+			english, icon, code
+		] = attributes.map( attr => infobox.getParameter( attr )?.value )
+		if ( !english || !icon || !code ) return null
+		return [
+			english,
+			'carta mágica',
+			'mágica',
+			icon.toLowerCase(),
+			code
+		]
+	} else if ( cardType === 'carta de trampa' ) {
+		const attributes = [
+			'inglés', 'icono', 'código'
+		]
+		const [
+			english, icon, code
+		] = attributes.map( attr => infobox.getParameter( attr )?.value )
+		if ( !english || !icon || !code ) return null
+		return [
+			english,
+			'carta de trampa',
+			'trampa',
+			icon.toLowerCase(),
+			code
+		]
+	} else {
+		const attributes = [
+			'inglés', 'atributo', 'tipo', 'nivel', 'ataque', 'defensa', 'código'
+		]
+		const [
+			english, attribute, type, level, attack, defense, code
+		] = attributes.map( attr => infobox.getParameter( attr )?.value )
+		if ( !english || !attribute || !type || !level || !attack || !code ) return null
+		if ( cardType === 'monstruo de enlace' ) {
+			return [
+				english,
+				cardType,
+				attribute,
+				type,
+				level,
+				attack,
+				code
+			]
+		} else {
+			if ( !defense ) return null
+			return [
+				english,
+				cardType,
+				attribute,
+				type,
+				level,
+				attack,
+				defense,
+				code
+			]
+		}
+	}
+}
+
 void ( async () => {
 	const fandom = new Fandom()
 	const wiki = fandom.getWiki( 'es.yugioh' )
 	const cards = ( await wiki.getTransclusions( 'Plantilla:Infobox Carta' ) ).sort()
 
-	let counter = 0
-	const total = cards.length
-	console.time( 'a' )
+	const data: Record<string, string[]> = {}
+
 	for await ( const page of wiki.iterPages( cards ) ) {
-		console.log( page.title )
-		counter++
 		const content = page.revisions[ 0 ]?.slots.main.content
-		if ( !content ) {
-			console.log( `${ counter } / ${ total }`, `No content: ${ page.title }` )
-			break
-		}
+		if ( !content ) continue
 		const parsed = parse( content )
 		const [ infobox ] = parsed.findTemplate( 'Infobox Carta' ).nodes
-		if ( !infobox ) {
-			console.log( `${ counter } / ${ total }`, `No infobox: ${ page.title }` )
-			console.log( util.inspect( parsed, {
-				colors: true,
-				depth: 6
-			} ) )
-			break
+		if ( !infobox ) continue
+		const cardData = parseCard( infobox )
+		if ( cardData ) {
+			const es = getIdentifier( page.title )
+			const en = cardData[ 0 ] ? getIdentifier( cardData[ 0 ] ) : null
+			const fullData = [
+				page.title, ...cardData
+			]
+			data[ es ] = fullData
+			if ( en ) data[ en ] = fullData
 		}
-		console.log( `${ counter } / ${ total }`, infobox.getParameter( 'inglés' )?.value )
 	}
-	console.timeEnd( 'a' )
+
+	const filepath = path.resolve( __dirname, '../cards.json' )
+	fs.ensureFileSync( filepath )
+	fs.writeJSONSync( filepath, data, {
+		spaces: '\t'
+	} )
 } )()
