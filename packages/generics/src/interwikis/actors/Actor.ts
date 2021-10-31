d90d279d5f17f015197e08ffd41815ef1e110732
bitomic <35199700+bitomic@users.noreply.github.com>
2021-10-31 12:43:31 -0600

    refactor: divide some methods from interwiki remover

--- packages/generics/src/interwikis/actors/Actor.ts
+++ packages/generics/src/interwikis/actors/Actor.ts
@@ -1,10 +1,14 @@
-import type { FandomBot, Wiki } from 'mw.js'
+/* eslint-disable sort-imports */
 import type { WikiWithLang } from '../utils'
+import { escapeRegex, sleep } from '../utils'
+import type { FandomBot, Wiki } from 'mw.js'
 
 export abstract class InterwikiActor {
 	protected allowedLanguages: Set<string>
 	protected bot: FandomBot
 	protected family: Record<string, WikiWithLang>
+
+	protected static INTERWIKI_REGEX = /\[\[([a-z-]+):(.+)(\|.*?)?\]\]/gi
 
 	public constructor( { allowedLanguages, bot, family }: { allowedLanguages: string[], bot: FandomBot, family: Record<string, WikiWithLang> } ) {
 		this.allowedLanguages = new Set( allowedLanguages )
@@ -13,6 +17,27 @@ export abstract class InterwikiActor {
 	}
 
 	public abstract execute( params: { doEdit?: boolean } ): Promise<void>
+
+	protected async edit( { doEdit, log, summary, text, title }: { doEdit: boolean, log: string, summary: string, text: string, title: string } ): Promise<boolean> {
+		console.log( log )
+		if ( doEdit ) {
+			const result = await this.bot.edit( {
+				bot: true,
+				summary,
+				text,
+				title
+			} )
+				.then( () => true )
+				.catch( e => {
+					console.error( `An error occurred while processing ${ title }` )
+					console.error( e )
+					return false
+				} )
+			await sleep( 2000 )
+			return result
+		}
+		return false
+	}
 
 	protected async *iterPagesById( wiki: Wiki, ids: Array<string | number> ): ReturnType<Wiki[ 'iterPages' ]> {
 		while ( ids.length !== 0 ) {
@@ -37,5 +62,23 @@ export abstract class InterwikiActor {
 				yield page as any // sorry not sorry
 			}
 		}
+	}
+
+	protected static getDataFromId( id: string ): [ string, string ] {
+		const arr = id.split( /-/g )
+		const pageid = arr.pop() ?? ''
+		const lang = arr.join( '-' )
+		return [
+			lang, pageid
+		]
+	}
+
+	protected static normalizeInterwiki( lang: string, target: string ): string {
+		return `${ lang }:${ encodeURIComponent( target.replace( / /g, '_' ) ) }`
+	}
+
+	protected static regexify( lang: string, title: string ): RegExp {
+		const target = encodeURIComponent( title.replace( / /g, '_' ) )
+		return new RegExp( ` *${ lang } *: *${ escapeRegex( target ) }`, 'gi' )
 	}
 }
--- packages/generics/src/interwikis/actors/Remover.ts
+++ packages/generics/src/interwikis/actors/Remover.ts
@@ -1,32 +1,11 @@
-import { escapeRegex, sleep } from '../utils'
-import type { FandomBot, Wiki } from 'mw.js'
 import type { IInterwikisAttributes } from '../models'
+import { InterwikiActor } from './Actor'
 import { Interwikis } from '../models'
 import type { Model } from 'sequelize'
-import type { WikiWithLang } from '../utils'
 
-export class InterwikiRemover {
-	private allowedLanguages: Set<string>
-	private bot: FandomBot
-	private family: Record<string, WikiWithLang>
-
-	public constructor( { allowedLanguages, bot, family }: { allowedLanguages: string[], bot: FandomBot, family: Record<string, WikiWithLang> } ) {
-		this.allowedLanguages = new Set( allowedLanguages )
-		this.bot = bot
-		this.family = family
-	}
-
+export class InterwikiRemover extends InterwikiActor {
 	public async execute( { doEdit = true }: { doEdit?: boolean } ): Promise<void> {
-		const nullies = ( await InterwikiRemover.getNullies() )
-			.filter( i => this.allowedLanguages.has( i.getDataValue( 'fromLang' ) ) )
-			.reduce( ( collection, item ) => {
-				const lang = item.getDataValue( 'fromLang' )
-				if ( lang in this.family ) {
-					if ( !collection[ lang ] ) collection[ lang ] = []
-					collection[ lang ]?.push( item )
-				}
-				return collection
-			}, {} as Record<string, Array<Model<IInterwikisAttributes, IInterwikisAttributes>>> )
+		const nullies = await this.getNullies()
 
 		for ( const lang in nullies ) {
 			const wiki = this.family[ lang ]
@@ -44,76 +23,68 @@ export class InterwikiRemover {
 					const id = fromId.split( /-/g ).pop()
 					return id && id === `${ page.pageid }`
 				} )
-				let nullyInterwikis = pageNullies.map( i => {
+				const nullyInterwikis = pageNullies.map( i => {
 					const lang = i.getDataValue( 'toLang' )
 					const target = encodeURIComponent( i.getDataValue( 'toTitle' ).replace( / /g, '_' ) )
-					return new RegExp( ` *${ lang } *: *${ escapeRegex( target ) }`, 'gi' )
-				} )
-				const regex = /\[\[([a-z-]+):(.+)(\|.*?)?\]\]/gi
-
-				const removed: string[] = []
-				const newContent = content.replace( regex, ( original: string, lang: string, target: string  ) => {
-					const text = `${ lang }:${ encodeURIComponent( target.replace( / /g, '_' ) ) }`
-					const someMatch = nullyInterwikis.find( r => text.match( r ) )
-					if ( someMatch ) {
-						nullyInterwikis = nullyInterwikis.filter( i => i !== someMatch )
-						removed.push( original )
-						return ''
-					}
-					return original
+					return InterwikiRemover.regexify( lang, target )
 				} )
 
-				if ( removed.length === 0 ) continue
-
-				const summary = `Removing interwikis: ${ removed.join( ', ' ) }`
-				console.log( `Updating [[${ lang }:${ page.title }]]: ${ summary }` )
-				if ( doEdit ) {
-					await this.bot.edit( {
-						bot: true,
-						summary,
-						text: newContent,
-						title: page.title
-					} )
-						.catch( e => {
-							console.error( `An error occurred while processing ${ page.title }` )
-							console.error( e )
-						} )
-					await sleep( 2000 )
-				}
+				await this.removeInterwikis( {
+					content,
+					doEdit,
+					lang,
+					regexes: nullyInterwikis,
+					title: page.title
+				} )
 			}
 		}
 	}
 
-	private static getNullies(): Promise<Array<Model<IInterwikisAttributes, IInterwikisAttributes>>> {
-		return Interwikis.findAll( {
+	private async getNullies(): Promise<Record<string, Array<Model<IInterwikisAttributes, IInterwikisAttributes>>>> {
+		const query = await Interwikis.findAll( {
 			where: {
 				toId: null
 			}
 		} )
+		return query.filter( i => this.allowedLanguages.has( i.getDataValue( 'fromLang' ) ) )
+			.reduce( ( collection, item ) => {
+				const lang = item.getDataValue( 'fromLang' )
+				if ( lang in this.family ) {
+					if ( !collection[ lang ] ) collection[ lang ] = []
+					collection[ lang ]?.push( item )
+				}
+				return collection
+			}, {} as Record<string, Array<Model<IInterwikisAttributes, IInterwikisAttributes>>> )
 	}
 
-	private async *iterPagesById( wiki: Wiki, ids: Array<string | number> ): ReturnType<Wiki[ 'iterPages' ]> {
-		while ( ids.length !== 0 ) {
-			const res = await wiki.get<{
-				query: {
-					pages: Array<Record<string, unknown>>
-				}
-			}>( {
-				action: 'query',
-				pageids: ids.splice( 0, 50 ).join( '|' ),
-				prop: 'revisions',
-				rvprop: 'content',
-				rvslots: 'main'
-			} )
+	// eslint-disable-next-line require-await
+	protected async removeInterwikis( { content, doEdit, lang, regexes, title }: { content: string, doEdit: boolean, lang: string, regexes: RegExp[], title: string } ): Promise<boolean> {
+		const removed: string[] = []
+		const newContent = InterwikiRemover.removeInterwikisFromText( content, regexes, removed )
 
-			for ( const page of res.query.pages ) {
-				if ( 'missing' in page ) {
-					continue
-				}
+		if ( removed.length === 0 ) return false
 
-				// eslint-disable-next-line @typescript-eslint/no-explicit-any
-				yield page as any // sorry not sorry
+		const summary = `Removing interwikis: ${ removed.join( ', ' ) }`
+		return this.edit( {
+			doEdit,
+			log: `Updating [[${ lang }:${ title }]]: ${ summary }`,
+			summary,
+			text: newContent,
+			title: title
+		} )
+	}
+
+	protected static removeInterwikisFromText( content: string, regexes: RegExp[], removed: string[] ): string {
+		const executor = ( original: string, lang: string, target: string  ): string => {
+			const text = InterwikiRemover.normalizeInterwiki( lang, target )
+			const someMatch = regexes.find( r => text.match( r ) )
+			if ( someMatch ) {
+				regexes = regexes.filter( i => i !== someMatch )
+				removed.push( original )
+				return ''
 			}
+			return original
 		}
+		return content.replace( InterwikiRemover.INTERWIKI_REGEX, executor )
 	}
 }
