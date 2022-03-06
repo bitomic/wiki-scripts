6f01905faa9d605b5819ea9d9a777fd488e5f5cd
bitomic <35199700+bitomic@users.noreply.github.com>
2022-03-06 14:24:52 -0600

    feat(workspaces): core

---
+++ packages/core/src/lib/Consumer.ts
@@ -0,0 +1,33 @@
+import { env } from '@wiki-scripts/shared'
+import { Fandom } from 'mw.js'
+import type { FandomBot } from 'mw.js'
+
+export interface ConsumerOptions {
+	name: string
+}
+
+export interface ConsumerResult {
+	message?: string
+	success: boolean
+}
+
+export abstract class Consumer {
+	protected readonly fandom: Fandom
+	public readonly name: string
+
+	public constructor( options: ConsumerOptions ) {
+		this.fandom = new Fandom( {
+			cookies: 'cookies.json'
+		} )
+		this.name = options.name
+	}
+
+	public abstract run( message: unknown ): ConsumerResult | Promise<ConsumerResult>
+
+	protected getFandomBot(): Promise<FandomBot> {
+		return this.fandom.login( {
+			password: env.FANDOM_PASSWORD,
+			username: env.FANDOM_USERNAME
+		} )
+	}
+}
